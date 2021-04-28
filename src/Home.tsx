a5b2c0195de3c5badccd9748888d040ea4489090
Elliot <elliotreborn@gmail.com>
2021-04-28 14:36:38 +0800

    add image

--- src/Home.tsx
+++ src/Home.tsx
@@ -75,16 +75,6 @@ function Home() {
           <Link to="/application-ui/form/SignIn">SignIn</Link>
         </p>
       </div>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="float-right hover:float-left ">
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
